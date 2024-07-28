eb80da8e08efa40e34b57d3df992c9a5f4fa8ceb
Eyüp Kaan Özteriş <68303307+EyupKaan@users.noreply.github.com>
2024-07-28 16:19:51 +0300

    Hibernate applied and executed

--- JPA-ORM/src/main/java/com/eyupkaan/Main.java
+++ JPA-ORM/src/main/java/com/eyupkaan/Main.java
@@ -1,7 +1,28 @@
 package com.eyupkaan;
 
+import com.eyupkaan.entity.Customer;
+import com.eyupkaan.util.Hibernateutil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.type.descriptor.java.spi.JsonJavaType;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Customer me = new Customer("Eyüp Kaan", "Özteriş");
+
+        Session session = Hibernateutil.getSessionFactory().openSession();
+
+        try (session) {
+            Transaction transaction = null;
+            transaction = session.beginTransaction();
+            session.persist(me);
+            transaction.commit();
+
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+
     }
 }
