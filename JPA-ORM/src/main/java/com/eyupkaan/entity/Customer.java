7b4b75178eaeca9f817a87b19d434227e884c1ad
Eyüp Kaan Özteriş <68303307+EyupKaan@users.noreply.github.com>
2024-07-28 14:35:18 +0300

    Customer covered as Entity

--- JPA-ORM/src/main/java/com/eyupkaan/entity/Customer.java
+++ JPA-ORM/src/main/java/com/eyupkaan/entity/Customer.java
@@ -1,18 +1,26 @@
-package com.eyupkaan;
+package com.eyupkaan.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(schema = "edudb", name = "T_CUSTOMERS")
 public class Customer {
     @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CUSTOMER_ID")
     private Long id;
 
     @Getter
     @Setter
+    @Column(name = "FIRST_NAME", length = 55, nullable = false)
     private String firstName;
 
     @Getter
     @Setter
+    @Column(name = "LAST_NAME", length = 30, nullable = false)
     private String lastName;
 
     public Customer(){};
